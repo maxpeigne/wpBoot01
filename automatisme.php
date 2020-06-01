373bfafcb311a183bbcce9d9166deb2ebb681527
maxpeigne <peigneanthony@hotmail.fr>
2020-06-01 15:11:58 +0200

    Fin de la partie 2 d'openclassroom

--- automatisme.php
+++ automatisme.php
@@ -1,30 +1,44 @@
-<?php get_header(); ?>
-
 <div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Automacile</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+  <a class="navbar-brand" href="#">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" width="40" height="30" alt="Site Logo">
+      Automacile</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
         <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="automatisme.php">Automatisme</a>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo get_stylesheet_directory_uri(); ?>/automatisme.php">Automatisme</a>
       </li>
     </ul>
   </div>
 </nav>
 </div>
+<div>
+<div class="container">
+    <div class="jumbotron">
+    
+        <h1>Bienvenue sur Automacile !</h1>
+    </div>                       
+</div>
     
 <div class="container">
-    <p>
-
-        Cognitis enim pilatorum caesorumque funeribus nemo deinde ad has stationes appulit navem, sed ut Scironis praerupta letalia declinantes litoribus Cypriis contigui navigabant, quae Isauriae scopulis sunt controversa.
+    <div class="card">
+        <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/images/instruction.png" alt="Certificat">
+        <div class="card-body">
+            <h5 class="card-title">Titre 01</h5>
+            <p class="card-text">Contenu de la carte 01</p>
+        </div>
+    </div>
+</div>
+    
+<div class="container">
+><p>   
+        Ob haec et huius modi multa, quae cernebantur in paucis, omnibus timeri sunt coepta. et ne tot malis dissimulatis paulatimque serpentibus acervi crescerent aerumnarum, nobilitatis decreto legati mittuntur: Praetextatus ex urbi praefecto et ex vicario Venustus et ex consulari Minervius oraturi, ne delictis supplicia sint grandiora, neve senator quisquam inusitato et inlicito more tormentis exponeretur.
     </p>
 </div>
-
-<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -2,35 +2,22 @@
 
 <div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Automacile</a>
+  <a class="navbar-brand" href="#">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" width="40" height="30" alt="Site Logo">
+      Automacile</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  <!--  <ul class="navbar-nav">
+    <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="automatisme.php">Automatisme</a>
+        <a class="nav-link" href="<?php echo get_stylesheet_directory_uri(); ?>/automatisme.php">Automatisme</a>
       </li>
-    </ul> -->
-      <ul class="nav justify-content-center">
-          <li class="nav-item">
-              <a class="nav-link active" href="#">Active</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-      </ul>
-
+    </ul>
   </div>
 </nav>
 </div>
@@ -43,14 +30,44 @@
 </div>
     
 <div class="container">
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Titre 01</h5>
-            <p class="card-text">Contenu de la carte 01</p>
+    <div class="card-deck">
+        <div class="card align">
+            <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/images/instruction.png" alt="Certificat">
+            <div class="card-body bg-primary text-white font-weight-bold">
+                <h4 class="card-title">Titre 01</h4>
+                <p class="card-text">Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01Contenu de la carte 01v</p>
+                <a href="#" class="btn btn-secondary btn-block stretched-link">See profile</a>
+            </div>
+        </div>
+        <div class="card ">
+            <div class="card-body bg-danger text-white font-weight-bold">
+                <h5 class="card-title">Titre 02</h5>
+                <p class="card-text">Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02Contenu de la carte 02</p>
+                <a href="#" class="btn btn-secondary btn-block stretched-link">See profile</a>
+            </div>
+            <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/images/certificate.png" alt="Certificat">
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/images/job.png" alt="Certificat">
+            <div class="card-body bg-success text-white font-weight-bold">
+                <h5 class="card-title">Titre 03</h5>
+                <p class="card-text">Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte  de la carte 03Contenu de la carte 03Contenu de la carte 03 la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03Contenu de la carte 03</p>
+                <a href="#" class="btn btn-secondary btn-block stretched-link">See profile</a>
+            </div>
         </div>
     </div>
 </div>
-    
+<div class="container">
+    <p></p>
+</div>
+<div class="container">
+    <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/3y2ahAvV6XY" allowfullscreen></iframe>
+    </div>
+</div>
+<div class="container">
+    <p></p>
+</div>
 <div class="container">
     <ul class="list-inline">
         <li class="list-inline-item">Je</li>
@@ -58,20 +75,7 @@
         <li class="list-inline-item">Une Liste</li>
         <li class="list-inline-item">Inline</li>
     </ul>
-    <ul class="nav justify-content-center">
-          <li class="nav-item">
-              <a class="nav-link active" href="#">Active</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-     </ul>
+
     <p>    
         Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.
      </p><p>   
